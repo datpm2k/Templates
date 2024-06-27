fe872deaf3ae547e17a0c878fad8f7aaef3b886b
tad <datpm1@hdbank.com.vn>
2024-06-27 16:52:58 +0700

    :beers: add tempalte

---
+++ posivitus/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,css}"],
+  theme: {
+    colors: {
+      dark: "#191A23",
+      green: "#B9FF66",
+      grey: "#F3F3F3",
+      white: "#FFFFFF",
+      black: "#000000"
+    },
+    fontFamily: {
+      body: "Space Grotesk",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
